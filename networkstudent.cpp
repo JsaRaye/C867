06f87f1a0dd27c4546e3906a7e14b4cc69a250bf
JsaRaye <55034239+JsaRaye@users.noreply.github.com>
2020-03-14 17:27:17 -0400

    Create networkstudent.cpp

---
+++ networkstudent.cpp
@@ -0,0 +1,28 @@
+#include "student.h"
+#include "networkstudent.h"
+#include "degree.h"
+#include <iostream>
+using namespace std;
+
+
+NetworkStudent::NetworkStudent()
+{
+
+}
+
+NetworkStudent::NetworkStudent(string sID, string fName, string lName, string eADD, int sAge, int* nDays, Degree degreeProgram)
+{
+	degreeProgram = degreeProgram;
+}
+
+Degree NetworkStudent::getDegreeProgram()
+{
+	return degreeProgram;
+}
+
+void NetworkStudent::print()
+{
+	cout << "\tFirst Name: " << getFirstName() << "\tLast Name: " << getLastName() << "\tAge: " << getAge()
+		<< "\tDays In Course: {" << getNumDays()[0] << ", " << getNumDays()[1] << ", " << getNumDays()[2] << "} "
+		<< "Degree Program: Network" << endl;
+}
