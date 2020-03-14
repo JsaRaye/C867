f9f88240dcd397982609ef2139bd20e50723c4c0
JsaRaye <55034239+JsaRaye@users.noreply.github.com>
2020-03-14 17:25:39 -0400

    Create securitystudent.cpp
    Two warnings on this file. Line 8/13 - degreeProgram is uninitialized.

---
+++ securitystudent.cpp
@@ -0,0 +1,28 @@
+#include "student.h"
+#include "securitystudent.h"
+#include "degree.h"
+#include <iostream>
+using namespace std;
+
+
+SecurityStudent::SecurityStudent()
+{
+
+}
+
+SecurityStudent::SecurityStudent(string sID, string fName, string lName, string eADD, int sAge, int* nDays, Degree degreeProgram)
+{
+	degreeProgram = degreeProgram;
+}
+
+Degree SecurityStudent::getDegreeProgram()
+{
+	return degreeProgram;
+}
+
+void SecurityStudent::print()
+{
+	cout << "\tFirst Name: " << getFirstName() << "\tLast Name: " << getLastName() << "\tAge: " << getAge()
+		<< "\tDays In Course: {" << getNumDays()[0] << ", " << getNumDays()[1] << ", " << getNumDays()[2] << "} "
+		<< "Degree Program: Security" << endl;
+}
