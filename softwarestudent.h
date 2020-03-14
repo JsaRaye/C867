23220dfcbc20e16b6346b541860d5c32ff89102d
JsaRaye <55034239+JsaRaye@users.noreply.github.com>
2020-03-14 17:27:46 -0400

    Create softwarestudent.h

---
+++ softwarestudent.h
@@ -0,0 +1,21 @@
+#pragma once
+#include "student.h"
+#include <iostream>
+#include <string>
+using namespace std;
+
+class SoftwareStudent : public Student
+{
+public:
+	SoftwareStudent();
+
+	SoftwareStudent(string sID, string fName, string lName, string eADD, int sAge, int* nDays, Degree degreeProgram);
+
+	Degree getDegreeProgram() override;
+
+	void print() override;
+
+private:
+	Degree degreeProgram;
+};
+
