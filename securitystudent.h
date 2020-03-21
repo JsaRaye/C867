5cea690b47d4e99a3974569ba7b4deca18cfb41d
JsaRaye <55034239+JsaRaye@users.noreply.github.com>
2020-03-21 12:42:59 -0400

    Update securitystudent.h

--- securitystudent.h
+++ securitystudent.h
@@ -1,20 +1,17 @@
 #pragma once
 #include "student.h"
-#include <iostream>
-#include <string>
-using namespace std;
 
 class SecurityStudent : public Student
 {
 public:
 	SecurityStudent();
-
-	SecurityStudent(string sID, string fName, string lName, string eADD, int sAge, int* numDays, Degree degreeProgram);
+	
+	SecurityStudent(string sID, string fName, string lName, string eADD, int age, int* numDays, Degree degreeProgram);
 
 	Degree getDegreeProgram() override;
 
 	void print() override;
 
 private:
-	Degree degreeProgram;
+	Degree degreeProgram; //Degree::SECURITY
 };
