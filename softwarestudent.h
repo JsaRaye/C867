68363653d84c7b33e24d22d955ab250be345f43a
JsaRaye <55034239+JsaRaye@users.noreply.github.com>
2020-03-21 12:44:49 -0400

    Update softwarestudent.h

--- softwarestudent.h
+++ softwarestudent.h
@@ -1,21 +1,17 @@
 #pragma once
 #include "student.h"
-#include <iostream>
-#include <string>
-using namespace std;
 
 class SoftwareStudent : public Student
 {
 public:
 	SoftwareStudent();
 
-	SoftwareStudent(string sID, string fName, string lName, string eADD, int sAge, int* numDays, Degree degreeProgram);
+	SoftwareStudent(string sID, string fName, string lName, string eADD, int age, int* numDays, Degree degreeProgram);
 
 	Degree getDegreeProgram() override;
 
 	void print() override;
 
 private:
-	Degree degreeProgram;
+	 Degree degreeProgram; //Degree::SOFTWARE??
 };
-
